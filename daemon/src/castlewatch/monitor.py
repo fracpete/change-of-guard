2e1e7d167360d880d36c1da64629ec07507a6c09
Peter Reutemann <fracpete@gmail.com>
2014-09-16 22:36:06 +1200

    using timers now for obtaining images; added motion detection

--- daemon/src/castlewatch/monitor.py
+++ daemon/src/castlewatch/monitor.py
@@ -15,24 +15,71 @@
 # Copyright (C) 2014 Fracpete (fracpete at gmail dot com)
 
 import config as cfg
+import sys
 import os
 import urllib
+import daemon
 from datetime import datetime
+from threading import Timer
+import motion
 
-# load config
-config = cfg.load_config()
-monitors = cfg.get_monitors(config)
 
-# create output dirs if necessary
-for monitor in monitors:
-    outDir = monitors[monitor]['output']
-    if not os.path.exists(outDir):
-        os.makedirs(outDir)
+""" the configuration """
+config = None
 
-# create timestamp
-ts = datetime.now()
-filename = ts.strftime(config['general']['timestamp'])
+""" the monitors """
+monitors = None
 
-# retrieve images
-for monitor in monitors:
-    urllib.urlretrieve(monitors[monitor]['url'], monitors[monitor]['output'] + os.sep + filename)
+""" recent image list per monitor """
+recent_images = {}
+
+
+def get_image(monitor):
+    """
+    Retrieves the image for the defined monitor.
+    :param monitor: the monitor
+    :type monitor: str
+    """
+    global config, monitors, recent_images
+    ts = datetime.now()
+    filename = ts.strftime(config['general']['timestamp'])
+    # paused?
+    out_dir = monitors[monitor]['output']
+    out_file = out_dir + os.sep + filename
+    if not os.path.exists(out_dir + os.sep + "PAUSED"):
+        urllib.urlretrieve(monitors[monitor]['url'], out_file)
+        recent_images[monitor].insert(0, out_file)
+        recent_images[monitor] = recent_images[monitor][:2]
+        # detect motion?
+        if monitors[monitor]['detect_motion'] and (len(recent_images[monitor]) == 2):
+            detected = motion.detect_motion(recent_images[monitor][0], recent_images[monitor][1], monitors[monitor]['threshold'])
+            # delete old file?
+            if not detected:
+                os.remove(recent_images[monitor][1])
+    Timer(monitors[monitor]['interval'], get_image, args=[monitor]).start()
+
+
+def main():
+    # load config
+    global config, monitors, recent_images
+    config = cfg.load_config()
+    monitors = cfg.get_monitors(config)
+
+    # create output dirs if necessary
+    for monitor in monitors:
+        out_dir = monitors[monitor]['output']
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+
+    # retrieve images
+    for monitor in monitors:
+        print "Starting monitor:", monitor
+        recent_images[monitor] = []
+        Timer(monitors[monitor]['interval'], get_image, args=[monitor]).start()
+
+if __name__ == '__main__':
+    if "-d" in sys.argv:
+        with daemon.DaemonContext():
+            main()
+    else:
+        main()
--- daemon/src/castlewatch/motion.py
+++ daemon/src/castlewatch/motion.py
@@ -57,36 +57,47 @@ def count_diff(img):
 def detect_motion(t0, t1, threshold):
     """
     Returns true if there was motion detected between the two images.
-    :param t0: the first image
-    :param t1: the second image
+    :param t0: the first image (image/filename)
+    :param t1: the second image (image/filename)
     :param threshold: the threshold in percent (0-1)
     :type threshold: float
     """
+    if isinstance(t0, basestring):
+        t0 = load_img(t0)
+    if isinstance(t1, basestring):
+        t1 = load_img(t1)
     size  = t0.size
     count = count_diff(diff_img(t0, t1))
     ratio = float(count) / float(size)
+    if ratio > threshold:
+        print ratio
     return ratio > threshold
 
-# load config
-config = cfg.load_config()
 
-# motion?
-monitors = cfg.get_monitors(config)
-for monitor in monitors:
-    print monitor
-    l = []
-    d = monitors[monitor]['output']
-    for f in os.listdir(d):
-        full = os.path.join(d, f)
-        if os.path.isfile(full):
-            l.append(full)
-    l.sort()
-    if len(l) > 1:
-        t_minus = None
-        t_now = None
-        for i in xrange(len(l)):
-            t_minus = t_now
-            t_now = load_img(l[i])
-            if not (t_minus is None) and not (t_now is None):
-                motion = detect_motion(t_minus, t_now, monitors[monitor]['threshold'])
-                print "  ", os.path.basename(l[i-1]), os.path.basename(l[i]), motion
+def main():
+    # load config
+    config = cfg.load_config()
+
+    # motion?
+    monitors = cfg.get_monitors(config)
+    for monitor in monitors:
+        print monitor
+        l = []
+        d = monitors[monitor]['output']
+        for f in os.listdir(d):
+            full = os.path.join(d, f)
+            if os.path.isfile(full):
+                l.append(full)
+        l.sort()
+        if len(l) > 1:
+            t_minus = None
+            t_now = None
+            for i in xrange(len(l)):
+                t_minus = t_now
+                t_now = load_img(l[i])
+                if not (t_minus is None) and not (t_now is None):
+                    motion = detect_motion(t_minus, t_now, monitors[monitor]['threshold'])
+                    print "  ", os.path.basename(l[i-1]), os.path.basename(l[i]), motion
+
+if __name__ == '__main__':
+    main()
